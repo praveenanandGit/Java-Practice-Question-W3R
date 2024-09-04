b18493db89983e8b5076d1dc24f57cd57bb26e5d
Praveen Anand <48305458+praveenanandGit@users.noreply.github.com>
2024-09-04 16:16:20 +0530

    Create Basic2.java
    Write a Java program to print the sum of two numbers.
    Test Data:
    74 + 36
    Expected Output :
    110

---
+++ Basic2.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	     System.out.println("Enter Number A:");
+	     int A= sc.nextInt();
+	     System.out.println("Enter Number B:");
+	     int B= sc.nextInt();
+	     System.out.println(A+B);
+	}
+}
