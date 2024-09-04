61a8d57d430bca626ccd33895709003440bba996
Praveen Anand <48305458+praveenanandGit@users.noreply.github.com>
2024-09-04 13:02:44 +0530

    Create Basic1.java
    Java: Print hello and your name on a separate lines

---
+++ Basic1.java
@@ -0,0 +1,32 @@
+ //Write a Java program to print 'Hello' on screen and your name on a separate line.
+// Expected Output :
+// Hello
+// Alexandra Abramov
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Praveen Anand");
+	}
+}
+//Take input from user
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	    System.out.println("Enter Your Name:");
+	    String name=sc.nextLine();
+	    System.out.println("Hello World\n"+name);
+	}
+}
+
+
+
+
+
+
+
