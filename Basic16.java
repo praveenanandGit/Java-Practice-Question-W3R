94357290fcd90746c3886c9bb92b060cbe9eda24
Praveen Anand <48305458+praveenanandGit@users.noreply.github.com>
2024-09-05 18:23:14 +0530

    Create Basic16.java

---
+++ Basic16.java
@@ -0,0 +1,18 @@
+//Write a Java program to swap two variables.
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	      System.out.println("+\"\"\"\"\"\"+");  
+	      System.out.println("[| o o |]");
+	      System.out.println(" |  ^  |  ");
+          System.out.println(" | '-' | ");                            
+          System.out.println(" +-----+");                                    
+                                                
+
+	        
+	       
+	       
+	}
+}
