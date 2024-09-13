4fe5cbb13507e1cae7fb01f778a7430f8c559c30
Praveen Anand <48305458+praveenanandGit@users.noreply.github.com>
2024-09-13 21:50:42 +0530

    Create Randomno.java

---
+++ Randomno.java
@@ -0,0 +1,32 @@
+package basicPracticeQuestion;
+
+import java.util.Scanner;
+
+public class Basic5 {
+	
+		
+	public static void main(String[] args) {
+		int count=0;
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=4;j++) {
+				for(int k=1;k<=4;k++) {
+			if(k !=i && k!=j && i!=j) {
+				count++;
+				System.out.println(i+""+j+""+k);
+			}
+			
+		}
+			
+			
+			
+		}
+			
+					
+				}
+	    System.out.println("Total number of the three-digit-number is " + count);
+	}
+
+	
+	}
+
+
